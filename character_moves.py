f4a80d278dd6fd9d07e8dda4f0c7fa72e092a553
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-09-17 15:03:15 +0900

    삼각 운동 완성

--- character_moves.py
+++ character_moves.py
@@ -10,10 +10,11 @@ line = 1
 cx,cy = 10, 70
 time = 0.0
 PI = 3.141592
+
 while (True):
     clear_canvas()
 
-    if rTogle:
+    if rTogle == 1:
         if line == 1:
             cx += 5
             if cx > 780:
@@ -30,15 +31,35 @@ while (True):
             cy -= 5
             if cy < 70:
                 line = 1
-                rTogle = False
-    else:
+                rTogle = 2
+                cx, cy = 10, 70
+
+    elif rTogle == 2:
+        if line == 1:
+            cx += 5
+            if cx > 780:
+                line =2
+        elif line == 2:
+            cy += 5
+            cx -= 4
+            if cy > 550:
+                line = 3
+        elif line == 3:
+            cy -= 5
+            cx -= 4
+            if cx < 20:
+                line = 1
+                rTogle = 3
+
+    elif rTogle == 3:
         cx = 400 + 250 * math.cos(time*PI)
         cy = 300 + 250 * math.sin(time*PI)
         time += 0.02
         if(time >2):
             time = 0
-            rTogle = True
+            rTogle = 1
             cx,cy = 10,70
+
 
     delay(0.016)
     gr.draw_now(400, 10)
